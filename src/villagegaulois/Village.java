539d07e3203449f89cfb85d24c77d0bec67ab6dd
Alexandre Delarue <dll5774b@ensfsi.onmicrosoft.com>
2026-02-26 18:48:55 +0100

    Partie1

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -8,6 +8,69 @@ public class Village {
 	private Chef chef;
 	private Gaulois[] villageois;
 	private int nbVillageois = 0;
+	
+	private static class Marche{
+		private Etal[] etals;
+		
+		private Marche(int nbEtals) {
+			this.etals = new Etal[nbEtals];	
+		}
+		
+		private void utiliserEtal(int indiceEtal, Gaulois vendeur, String produit, int nbProduit) {
+			etals[indiceEtal].occuperEtal(vendeur,produit,nbProduit);
+		}
+		
+		private int trouverEtalLibre() {
+			for(int i=0;i<etals.length;i++) {
+				if(etals[i].isEtalOccupe()) {
+					return i;
+				}
+			}
+			return -1;
+		}
+		
+		private Etal[] trouverEtals(String produit) {
+			int nbEtalsProduit = 0;
+			for(int i=0;i<etals.length;i++) {
+				if(etals[i].contientProduit(produit)) {
+					nbEtalsProduit++;
+				}
+			}
+			Etal[] etalsProduit = new Etal[nbEtalsProduit];
+			int compteur = 0;
+			for(int j=0;j<etals.length;j++) {
+				if(etals[j].contientProduit(produit)) {
+					etalsProduit[compteur] = etals[j];
+					compteur++;
+				}
+			}
+			return etalsProduit;
+		}
+		
+		private Etal trouverVendeur(Gaulois vendeur) {
+			for(int i=0;i<etals.length;i++) {
+				Gaulois vendeurEtal = etals[i].getVendeur();
+				if(vendeurEtal.equals(vendeur)) {
+					return etals[i];
+				}
+			}
+			return null;
+		}
+		
+		private void afficherMarche() {
+			int nbEtalsVides = 0;
+			for(Etal etalaffiche:etals) {
+				if(etalaffiche.isEtalOccupe()) {
+					etalaffiche.afficherEtal();
+				}else {
+					nbEtalsVides++;
+				}
+			}
+			if(nbEtalsVides > 0) {
+				System.out.println("Il reste " + nbEtalsVides + " étals non utilisés dans le marché");
+			}
+		}
+	}
 
 	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
